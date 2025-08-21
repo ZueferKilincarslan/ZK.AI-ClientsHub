d99b21d3a4eeaeba93d1659a2d9575148820afe7
Züfer <z.kilincarslan@zk-ai.agency>
2025-08-21 21:11:43 +0200

    Remove incompatible vite-plugin-prerender

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import prerender from 'vite-plugin-prerender';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    prerender({
-      routes: [
-        '/',
-        '/workflows',
-        '/settings',
-        '/profile'
-      ],
-    }
-    )
     react()
   ],
   base: '/',
